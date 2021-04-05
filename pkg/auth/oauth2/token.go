5270be952b2e4fdc68e8cc183185ba854ec4bf05
Loo Zheng Yuan <me@zhengyuan.sg>
2021-04-05 23:25:02 +0800

    chore: add comment

--- pkg/auth/oauth2/token.go
+++ pkg/auth/oauth2/token.go
@@ -1,5 +1,7 @@
 package oauth2
 
+// TokenResponse represents the response type when querying
+// the token endpoint.
 type TokenResponse struct {
 	AccessToken  string `json:"access_token,omitempty"`
 	TokenType    string `json:"token_type,omitempty"`
