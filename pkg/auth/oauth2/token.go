37d8dddb746770b3d009336a6f20bf10a019197f
Loo Zheng Yuan <me@zhengyuan.sg>
2021-04-05 23:22:14 +0800

    feat: add oauth2 TokenResponse

---
+++ pkg/auth/oauth2/token.go
@@ -0,0 +1,8 @@
+package oauth2
+
+type TokenResponse struct {
+	AccessToken  string `json:"access_token,omitempty"`
+	TokenType    string `json:"token_type,omitempty"`
+	ExpiresIn    int    `json:"expires_in,omitempty"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+}
