a6337229376a2ce3dbe2117da8105b58abe1942b
Loo Zheng Yuan <me@zhengyuan.sg>
2021-04-07 21:40:46 +0800

    feat: add Store interface and generic errors

--- pkg/kv/kv.go
+++ pkg/kv/kv.go
@@ -1,2 +1,27 @@
 // Package kv provides interface for interacting with a key-value store.
 package kv
+
+import (
+	"errors"
+	"io"
+)
+
+var (
+	// ErrEmptyKey is returned when the key is empty.
+	ErrEmptyKey = errors.New("empty key")
+
+	// ErrNotFound is returned when the key does not exist on the key-value store.
+	ErrNotFound = errors.New("not found")
+)
+
+// Store is the interface for interacting with an key-value store.
+type Store interface {
+	// Push uploads data from a io.Reader object to the key-value store.
+	Push(key string, r io.Reader) error
+
+	// Pull downloads data from the store to a io.Writer object.
+	Pull(key string, w io.Writer) error
+
+	// Clear removes data associated with a given key.
+	Clear(key string) error
+}
