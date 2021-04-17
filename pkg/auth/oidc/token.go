1e4f28eb661e45c68d14eccf4acddef3d68e6584
Loo Zheng Yuan <me@zhengyuan.sg>
2021-04-17 12:39:11 +0800

    WIP

--- pkg/auth/oidc/token.go
+++ pkg/auth/oidc/token.go
@@ -1,6 +1,8 @@
 package oidc
 
 import (
+	"time"
+
 	"github.com/loozhengyuan/grench/pkg/auth/oauth2"
 )
 
@@ -9,4 +11,17 @@ import (
 type TokenResponse struct {
 	oauth2.TokenResponse
 	IDToken string `json:"id_token,omitempty"`
+}
+
+type IDToken struct {
+	Issuer              string    `json:"iss,omitempty"`
+	Subject             string    `json:"sub,omitempty"`
+	Audience            string    `json:"aud,omitempty"`
+	ExpiresAt           time.Time `json:"exp,omitempty"`
+	IssuedAt            int       `json:"iat,omitempty"`       // TODO: Use time.Time?
+	AuthTime            int       `json:"auth_time,omitempty"` // TODO: Use time.Time?
+	Nonce               string    `json:"nonce,omitempty"`
+	AuthClassReference  string    `json:"acr,omitempty"`
+	AuthMethodReference string    `json:"amr,omitempty"`
+	AuthorizedParty     string    `json:"azp,omitempty"`
 }
